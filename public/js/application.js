2b6022d10ccedc4b606d1040b3db84c416176003
anthony-ng <thomsonng123@gmail.com>
2015-01-21 16:30:52 -0800

    updated vote ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,25 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+// $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
-});
+//   $('.vote-button a').on('click', function(event) {
+
+//     event.preventDefault();
+
+//     console.log("hello")
+//   })
+// });
+
+  // var target = $(event.target);
+
+  // var voteRequest = $.ajax({
+  //   url: '/posts/:id/vote',
+  //   type: 'GET',
+  //   data: ''
+  // }).done(function() {
+  //   console.log("success");
+  // }).fail(function() {
+  //   console.log("fail");
+  // }).always(function() {
+  //   console.log("completed");
+  // });
+
